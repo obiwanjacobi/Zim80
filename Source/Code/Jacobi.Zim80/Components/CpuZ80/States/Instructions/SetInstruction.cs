1c7570a77230d3f13d452548686ae9b6fdd5deb2
Marc Jacobi <obiwanjacobi@hotmail.com>
2016-10-15 09:55:59 +0200

    Added SetInstruction with tests.

---
+++ Source/Code/Jacobi.Zim80/Components/CpuZ80/States/Instructions/SetInstruction.cs
@@ -0,0 +1,17 @@
+﻿namespace Jacobi.Zim80.Components.CpuZ80.States.Instructions
+{
+    internal class SetInstruction : SingleCycleInstruction
+    {
+        public SetInstruction(Die die) 
+            : base(die)
+        { }
+
+        protected override void OnLastCycleFirstM()
+        {
+            var bit = ExecutionEngine.Opcode.Definition.Y;
+            var reg = ExecutionEngine.Opcode.Definition.Register8FromZ;
+
+            Registers.PrimarySet[reg] = Die.Alu.SetBit(bit, Registers.PrimarySet[reg]);
+        }
+    }
+}
