c959114d5ce33d2eefed5cec8f00d44efc72e1c9
Marc Jacobi <obiwanjacobi@hotmail.com>
2016-10-15 10:19:58 +0200

    Added LoadRegister8Instruction with tests.

---
+++ Source/Code/Jacobi.Zim80.UnitTests/Components/CpuZ80/Instructions/LoadRegister8Instruction.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Jacobi.Zim80.Components.CpuZ80.Opcodes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jacobi.Zim80.Components.CpuZ80.UnitTests;
+using FluentAssertions;
+
+namespace Jacobi.Zim80.Components.CpuZ80.Instructions.UnitTests
+{
+    [TestClass]
+    public class LoadRegister8Instruction
+    {
+        private const byte Value = 0x55;
+
+        [TestMethod]
+        public void LD_reg8_reg8()
+        {
+            TestAll();
+        }
+
+        private void TestAll()
+        {
+            for (Register8Table trgReg = Register8Table.B; trgReg <= Register8Table.A; trgReg++)
+            {
+                if (trgReg == Register8Table.HL) continue;
+
+                for (Register8Table srcReg = Register8Table.B; srcReg <= Register8Table.A; srcReg++)
+                {
+                    if (srcReg == Register8Table.HL) continue;
+
+                    LD_Test(trgReg, srcReg);
+                }
+            }
+        }
+
+        private void LD_Test(Register8Table trgReg, Register8Table srcReg)
+        {
+            var ob = OpcodeByte.New(x: 1, z: (byte)srcReg, y: (byte)trgReg);
+            var cpuZ80 = ExecuteTest(ob, (cpu) => cpu.Registers.PrimarySet[srcReg] = Value);
+
+            cpuZ80.Registers.PrimarySet[trgReg].Should().Be(Value);
+        }
+
+        private CpuZ80 ExecuteTest(OpcodeByte ob, Action<CpuZ80> preTest)
+        {
+            var cpuZ80 = new CpuZ80();
+            var model = cpuZ80.Initialize(new[] { ob.Value });
+
+            cpuZ80.FillRegisters();
+            preTest(cpuZ80);
+
+            model.ClockGen.BlockWave(4);
+
+            return cpuZ80;
+        }
+    }
+}
---
+++ Source/Code/Jacobi.Zim80/Components/CpuZ80/States/Instructions/LoadRegister8Instruction.cs
@@ -0,0 +1,17 @@
+﻿namespace Jacobi.Zim80.Components.CpuZ80.States.Instructions
+{
+    internal class LoadRegister8Instruction : SingleCycleInstruction
+    {
+        public LoadRegister8Instruction(Die die) 
+            : base(die)
+        { }
+
+        protected override void OnLastCycleFirstM()
+        {
+            var srcReg = ExecutionEngine.Opcode.Definition.Register8FromZ;
+            var trgReg = ExecutionEngine.Opcode.Definition.Register8FromY;
+
+            Registers.PrimarySet[trgReg] = Registers.PrimarySet[srcReg];
+        }
+    }
+}
