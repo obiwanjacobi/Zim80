58a65a57653338c1135d62a2a084f855abafa131
Marc <obiwanjacobi@hotmail.com>
2016-11-24 19:07:34 +0100

    Added LoadSystemRegisterInstruction with tests.

---
+++ Source/Code/Jacobi.Zim80.UnitTests/Components/CpuZ80/Instructions/LoadSystemRegisterInstructionTest.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Jacobi.Zim80.Components.CpuZ80.Opcodes;
+using Jacobi.Zim80.Components.CpuZ80.UnitTests;
+using System;
+using FluentAssertions;
+
+namespace Jacobi.Zim80.Components.CpuZ80.Instructions.UnitTests
+{
+    [TestClass]
+    public class LoadSystemRegisterInstructionTest
+    {
+        private const byte Value = 0x55;
+
+        [TestMethod]
+        public void LdIA()
+        {
+            var ob = OpcodeByte.New(x: 1, z: 7, y: 0);
+
+            var cpu = ExecuteTest(ob, (z80) =>
+                {
+                    z80.Registers.A = Value;
+                });
+
+            cpu.AssertRegisters(a: Value, i: Value);
+        }
+
+        [TestMethod]
+        public void LdRA()
+        {
+            var ob = OpcodeByte.New(x: 1, z: 7, y: 1);
+
+            var cpu = ExecuteTest(ob, (z80) =>
+            {
+                z80.Registers.A = Value;
+            });
+
+            cpu.AssertRegisters(a: Value, r: Value);
+        }
+
+        [TestMethod]
+        public void LdAI()
+        {
+            var ob = OpcodeByte.New(x: 1, z: 7, y: 2);
+
+            var cpu = ExecuteTest(ob, (z80) =>
+            {
+                z80.Registers.Interrupt.IFF2 = true;
+                z80.Registers.I = Value;
+            });
+
+            cpu.AssertRegisters(a: Value, i: Value);
+            cpu.Registers.Flags.PV.Should().Be(true);
+        }
+
+        [TestMethod]
+        public void LdAR()
+        {
+            var ob = OpcodeByte.New(x: 1, z: 7, y: 3);
+
+            var cpu = ExecuteTest(ob, (z80) =>
+            {
+                z80.Registers.Interrupt.IFF2 = true;
+                z80.Registers.R = Value;
+            });
+
+            // R is incremented twice for the 2 opcode byte
+            cpu.AssertRegisters(a: Value + 2, r: Value + 2);
+            cpu.Registers.Flags.PV.Should().Be(true);
+        }
+
+        private static CpuZ80 ExecuteTest(OpcodeByte ob, Action<CpuZ80> preTest)
+        {
+            var cpuZ80 = new CpuZ80();
+            byte[] buffer = new byte[] { 0xED, ob.Value };
+
+            var model = cpuZ80.Initialize(buffer);
+
+            cpuZ80.FillRegisters();
+            preTest(cpuZ80);
+
+            model.ClockGen.BlockWave(9);
+
+            return cpuZ80;
+        }
+    }
+}
---
+++ Source/Code/Jacobi.Zim80/Components/CpuZ80/States/Instructions/LoadSystemRegisterInstruction.cs
@@ -0,0 +1,61 @@
+﻿namespace Jacobi.Zim80.Components.CpuZ80.States.Instructions
+{
+    internal class LoadSystemRegisterInstruction : SingleCycleInstruction
+    {
+        public LoadSystemRegisterInstruction(Die die) 
+            : base(die)
+        { }
+
+        protected override void OnLastCycleFirstM()
+        {
+            switch (ExecutionEngine.Opcode.Definition.Y)
+            {
+                case 0: //ld I, A
+                    LoadIntAcc();
+                    break;
+                case 1: //ld R, A
+                    LoadRefAcc();
+                    break;
+                case 2: //ld A, I
+                    LoadAccInt();
+                    break;
+                case 3: //ld A, R
+                    LoadAccRef();
+                    break;
+                default:
+                    throw Errors.AssignedToIllegalOpcode();
+            }
+        }
+
+        private void LoadIntAcc()
+        {
+            Registers.I = Registers.A;
+        }
+
+        private void LoadRefAcc()
+        {
+            Registers.R = Registers.A;
+        }
+
+        private void LoadAccInt()
+        {
+            Registers.A = Registers.I;
+            SetFlags(Registers.I);
+        }
+
+        private void LoadAccRef()
+        {
+            Registers.A = Registers.R;
+            SetFlags(Registers.R);
+        }
+
+        private void SetFlags(byte value)
+        {
+            Registers.Flags.S = Alu.IsNegative(value);
+            Registers.Flags.Z = Alu.IsZero(value);
+            Registers.Flags.H = false;
+            Registers.Flags.PV = Registers.Interrupt.IFF2;
+            Registers.Flags.N = false;
+        }
+    }
+}
